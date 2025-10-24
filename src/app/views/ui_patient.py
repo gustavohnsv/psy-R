6a22244e40b989498b8a829c07f6e31b3d37cbf3
gustvohnsv <guvi10vieira@gmail.com>
2025-10-24 08:44:56 -0300

    feat: improve patient screen

--- src/app/views/ui_patient.py
+++ src/app/views/ui_patient.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'pacient_screen.ui'
+## Form generated from reading UI file 'patient_screen.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.9.2
 ##
@@ -15,15 +15,16 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QDateEdit, QFormLayout, QGroupBox,
-    QHBoxLayout, QLabel, QLineEdit, QPushButton,
-    QSizePolicy, QSpacerItem, QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QApplication, QDateEdit, QFormLayout, QGridLayout,
+    QGroupBox, QHBoxLayout, QLabel, QLineEdit,
+    QPushButton, QSizePolicy, QSpacerItem, QVBoxLayout,
+    QWidget)
 
 class Ui_TelaPaciente(object):
     def setupUi(self, TelaPaciente):
         if not TelaPaciente.objectName():
             TelaPaciente.setObjectName(u"TelaPaciente")
-        TelaPaciente.resize(600, 400)
+        TelaPaciente.resize(800, 626)
         self.verticalLayout = QVBoxLayout(TelaPaciente)
         self.verticalLayout.setObjectName(u"verticalLayout")
         self.label_titulo = QLabel(TelaPaciente)
@@ -32,7 +33,7 @@ class Ui_TelaPaciente(object):
         font.setPointSize(16)
         font.setBold(True)
         self.label_titulo.setFont(font)
-        self.label_titulo.setAlignment(Qt.AlignCenter)
+        self.label_titulo.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         self.verticalLayout.addWidget(self.label_titulo)
 
@@ -61,28 +62,167 @@ class Ui_TelaPaciente(object):
 
         self.formLayout.setWidget(1, QFormLayout.ItemRole.FieldRole, self.dateEdit_nascimento)
 
-        self.label_responsavel = QLabel(self.groupBox)
-        self.label_responsavel.setObjectName(u"label_responsavel")
+        self.label_idade_crono = QLabel(self.groupBox)
+        self.label_idade_crono.setObjectName(u"label_idade_crono")
 
-        self.formLayout.setWidget(2, QFormLayout.ItemRole.LabelRole, self.label_responsavel)
+        self.formLayout.setWidget(2, QFormLayout.ItemRole.LabelRole, self.label_idade_crono)
 
-        self.lineEdit_responsavel = QLineEdit(self.groupBox)
-        self.lineEdit_responsavel.setObjectName(u"lineEdit_responsavel")
+        self.lineEdit_idade_crono = QLineEdit(self.groupBox)
+        self.lineEdit_idade_crono.setObjectName(u"lineEdit_idade_crono")
 
-        self.formLayout.setWidget(2, QFormLayout.ItemRole.FieldRole, self.lineEdit_responsavel)
+        self.formLayout.setWidget(2, QFormLayout.ItemRole.FieldRole, self.lineEdit_idade_crono)
 
-        self.label_escolaridade = QLabel(self.groupBox)
-        self.label_escolaridade.setObjectName(u"label_escolaridade")
+        self.label_escola = QLabel(self.groupBox)
+        self.label_escola.setObjectName(u"label_escola")
 
-        self.formLayout.setWidget(3, QFormLayout.ItemRole.LabelRole, self.label_escolaridade)
+        self.formLayout.setWidget(3, QFormLayout.ItemRole.LabelRole, self.label_escola)
 
-        self.lineEdit_escolaridade = QLineEdit(self.groupBox)
-        self.lineEdit_escolaridade.setObjectName(u"lineEdit_escolaridade")
+        self.lineEdit_escola = QLineEdit(self.groupBox)
+        self.lineEdit_escola.setObjectName(u"lineEdit_escola")
 
-        self.formLayout.setWidget(3, QFormLayout.ItemRole.FieldRole, self.lineEdit_escolaridade)
+        self.formLayout.setWidget(3, QFormLayout.ItemRole.FieldRole, self.lineEdit_escola)
+
+        self.label_turma = QLabel(self.groupBox)
+        self.label_turma.setObjectName(u"label_turma")
+
+        self.formLayout.setWidget(4, QFormLayout.ItemRole.LabelRole, self.label_turma)
+
+        self.lineEdit_turma = QLineEdit(self.groupBox)
+        self.lineEdit_turma.setObjectName(u"lineEdit_turma")
+
+        self.formLayout.setWidget(4, QFormLayout.ItemRole.FieldRole, self.lineEdit_turma)
 
 
         self.verticalLayout.addWidget(self.groupBox)
+
+        self.groupBox_2 = QGroupBox(TelaPaciente)
+        self.groupBox_2.setObjectName(u"groupBox_2")
+        self.gridLayout = QGridLayout(self.groupBox_2)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.groupBox_3 = QGroupBox(self.groupBox_2)
+        self.groupBox_3.setObjectName(u"groupBox_3")
+        self.groupBox_3.setEnabled(True)
+        self.formLayout_2 = QFormLayout(self.groupBox_3)
+        self.formLayout_2.setObjectName(u"formLayout_2")
+        self.label_resp1_nome = QLabel(self.groupBox_3)
+        self.label_resp1_nome.setObjectName(u"label_resp1_nome")
+
+        self.formLayout_2.setWidget(0, QFormLayout.ItemRole.LabelRole, self.label_resp1_nome)
+
+        self.label_resp1_escolaridade = QLabel(self.groupBox_3)
+        self.label_resp1_escolaridade.setObjectName(u"label_resp1_escolaridade")
+
+        self.formLayout_2.setWidget(5, QFormLayout.ItemRole.LabelRole, self.label_resp1_escolaridade)
+
+        self.lineEdit_resp1_nome = QLineEdit(self.groupBox_3)
+        self.lineEdit_resp1_nome.setObjectName(u"lineEdit_resp1_nome")
+
+        self.formLayout_2.setWidget(0, QFormLayout.ItemRole.FieldRole, self.lineEdit_resp1_nome)
+
+        self.lineEdit_resp1_escolaridade = QLineEdit(self.groupBox_3)
+        self.lineEdit_resp1_escolaridade.setObjectName(u"lineEdit_resp1_escolaridade")
+
+        self.formLayout_2.setWidget(5, QFormLayout.ItemRole.FieldRole, self.lineEdit_resp1_escolaridade)
+
+        self.label_resp1_idade = QLabel(self.groupBox_3)
+        self.label_resp1_idade.setObjectName(u"label_resp1_idade")
+
+        self.formLayout_2.setWidget(2, QFormLayout.ItemRole.LabelRole, self.label_resp1_idade)
+
+        self.label_resp1_profissao = QLabel(self.groupBox_3)
+        self.label_resp1_profissao.setObjectName(u"label_resp1_profissao")
+
+        self.formLayout_2.setWidget(3, QFormLayout.ItemRole.LabelRole, self.label_resp1_profissao)
+
+        self.lineEdit_resp1_idade = QLineEdit(self.groupBox_3)
+        self.lineEdit_resp1_idade.setObjectName(u"lineEdit_resp1_idade")
+
+        self.formLayout_2.setWidget(2, QFormLayout.ItemRole.FieldRole, self.lineEdit_resp1_idade)
+
+        self.lineEdit_resp1_profissao = QLineEdit(self.groupBox_3)
+        self.lineEdit_resp1_profissao.setObjectName(u"lineEdit_resp1_profissao")
+
+        self.formLayout_2.setWidget(3, QFormLayout.ItemRole.FieldRole, self.lineEdit_resp1_profissao)
+
+
+        self.gridLayout.addWidget(self.groupBox_3, 0, 0, 1, 1)
+
+        self.groupBox_4 = QGroupBox(self.groupBox_2)
+        self.groupBox_4.setObjectName(u"groupBox_4")
+        self.formLayout_3 = QFormLayout(self.groupBox_4)
+        self.formLayout_3.setObjectName(u"formLayout_3")
+        self.label_resp2_nome = QLabel(self.groupBox_4)
+        self.label_resp2_nome.setObjectName(u"label_resp2_nome")
+
+        self.formLayout_3.setWidget(0, QFormLayout.ItemRole.LabelRole, self.label_resp2_nome)
+
+        self.label_resp2_escolaridade = QLabel(self.groupBox_4)
+        self.label_resp2_escolaridade.setObjectName(u"label_resp2_escolaridade")
+
+        self.formLayout_3.setWidget(3, QFormLayout.ItemRole.LabelRole, self.label_resp2_escolaridade)
+
+        self.label_resp2_profissao = QLabel(self.groupBox_4)
+        self.label_resp2_profissao.setObjectName(u"label_resp2_profissao")
+
+        self.formLayout_3.setWidget(2, QFormLayout.ItemRole.LabelRole, self.label_resp2_profissao)
+
+        self.label_resp2_idade = QLabel(self.groupBox_4)
+        self.label_resp2_idade.setObjectName(u"label_resp2_idade")
+
+        self.formLayout_3.setWidget(1, QFormLayout.ItemRole.LabelRole, self.label_resp2_idade)
+
+        self.lineEdit_resp2_nome = QLineEdit(self.groupBox_4)
+        self.lineEdit_resp2_nome.setObjectName(u"lineEdit_resp2_nome")
+
+        self.formLayout_3.setWidget(0, QFormLayout.ItemRole.FieldRole, self.lineEdit_resp2_nome)
+
+        self.lineEdit_resp2_idade = QLineEdit(self.groupBox_4)
+        self.lineEdit_resp2_idade.setObjectName(u"lineEdit_resp2_idade")
+
+        self.formLayout_3.setWidget(1, QFormLayout.ItemRole.FieldRole, self.lineEdit_resp2_idade)
+
+        self.lineEdit_resp2_profissao = QLineEdit(self.groupBox_4)
+        self.lineEdit_resp2_profissao.setObjectName(u"lineEdit_resp2_profissao")
+
+        self.formLayout_3.setWidget(2, QFormLayout.ItemRole.FieldRole, self.lineEdit_resp2_profissao)
+
+        self.lineEdit_resp2_escolaridade = QLineEdit(self.groupBox_4)
+        self.lineEdit_resp2_escolaridade.setObjectName(u"lineEdit_resp2_escolaridade")
+
+        self.formLayout_3.setWidget(3, QFormLayout.ItemRole.FieldRole, self.lineEdit_resp2_escolaridade)
+
+
+        self.gridLayout.addWidget(self.groupBox_4, 0, 1, 1, 1)
+
+
+        self.verticalLayout.addWidget(self.groupBox_2)
+
+        self.groupBox_5 = QGroupBox(TelaPaciente)
+        self.groupBox_5.setObjectName(u"groupBox_5")
+        self.formLayout_4 = QFormLayout(self.groupBox_5)
+        self.formLayout_4.setObjectName(u"formLayout_4")
+        self.label_solicitante_nome = QLabel(self.groupBox_5)
+        self.label_solicitante_nome.setObjectName(u"label_solicitante_nome")
+
+        self.formLayout_4.setWidget(0, QFormLayout.ItemRole.LabelRole, self.label_solicitante_nome)
+
+        self.label_solicitante_crp = QLabel(self.groupBox_5)
+        self.label_solicitante_crp.setObjectName(u"label_solicitante_crp")
+
+        self.formLayout_4.setWidget(1, QFormLayout.ItemRole.LabelRole, self.label_solicitante_crp)
+
+        self.lineEdit_solicitante_nome = QLineEdit(self.groupBox_5)
+        self.lineEdit_solicitante_nome.setObjectName(u"lineEdit_solicitante_nome")
+
+        self.formLayout_4.setWidget(0, QFormLayout.ItemRole.FieldRole, self.lineEdit_solicitante_nome)
+
+        self.lineEdit_solicitante_crp = QLineEdit(self.groupBox_5)
+        self.lineEdit_solicitante_crp.setObjectName(u"lineEdit_solicitante_crp")
+
+        self.formLayout_4.setWidget(1, QFormLayout.ItemRole.FieldRole, self.lineEdit_solicitante_crp)
+
+
+        self.verticalLayout.addWidget(self.groupBox_5)
 
         self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
 
@@ -116,12 +256,27 @@ class Ui_TelaPaciente(object):
     def retranslateUi(self, TelaPaciente):
         TelaPaciente.setWindowTitle(QCoreApplication.translate("TelaPaciente", u"Form", None))
         self.label_titulo.setText(QCoreApplication.translate("TelaPaciente", u"Passo 2: Dados do Paciente", None))
-        self.groupBox.setTitle(QCoreApplication.translate("TelaPaciente", u"Informa\u00e7\u00f5es Pessoais", None))
+        self.groupBox.setTitle(QCoreApplication.translate("TelaPaciente", u"Informa\u00e7\u00f5es do paciente", None))
         self.label_nome.setText(QCoreApplication.translate("TelaPaciente", u"Nome Completo:", None))
         self.label_nascimento.setText(QCoreApplication.translate("TelaPaciente", u"Data de Nascimento:", None))
         self.dateEdit_nascimento.setDisplayFormat(QCoreApplication.translate("TelaPaciente", u"dd/MM/yyyy", None))
-        self.label_responsavel.setText(QCoreApplication.translate("TelaPaciente", u"Respons\u00e1vel:", None))
-        self.label_escolaridade.setText(QCoreApplication.translate("TelaPaciente", u"Escolaridade:", None))
+        self.label_idade_crono.setText(QCoreApplication.translate("TelaPaciente", u"Idade Cronol\u00f3gica:", None))
+        self.label_escola.setText(QCoreApplication.translate("TelaPaciente", u"Institui\u00e7\u00e3o de Ensino:", None))
+        self.label_turma.setText(QCoreApplication.translate("TelaPaciente", u"Turma:", None))
+        self.groupBox_2.setTitle(QCoreApplication.translate("TelaPaciente", u"Informa\u00e7\u00f5es dos respons\u00e1veis", None))
+        self.groupBox_3.setTitle("")
+        self.label_resp1_nome.setText(QCoreApplication.translate("TelaPaciente", u"1\u00b0 Respons\u00e1vel:", None))
+        self.label_resp1_escolaridade.setText(QCoreApplication.translate("TelaPaciente", u"Escolaridade:", None))
+        self.label_resp1_idade.setText(QCoreApplication.translate("TelaPaciente", u"Idade:", None))
+        self.label_resp1_profissao.setText(QCoreApplication.translate("TelaPaciente", u"Profiss\u00e3o:", None))
+        self.groupBox_4.setTitle("")
+        self.label_resp2_nome.setText(QCoreApplication.translate("TelaPaciente", u"2\u00b0 Respons\u00e1vel:", None))
+        self.label_resp2_escolaridade.setText(QCoreApplication.translate("TelaPaciente", u"Escolaridade:", None))
+        self.label_resp2_profissao.setText(QCoreApplication.translate("TelaPaciente", u"Profiss\u00e3o:", None))
+        self.label_resp2_idade.setText(QCoreApplication.translate("TelaPaciente", u"Idade:", None))
+        self.groupBox_5.setTitle(QCoreApplication.translate("TelaPaciente", u"Informa\u00e7\u00f5es da solicitante", None))
+        self.label_solicitante_nome.setText(QCoreApplication.translate("TelaPaciente", u"Nome Completo:", None))
+        self.label_solicitante_crp.setText(QCoreApplication.translate("TelaPaciente", u"CRP:", None))
         self.btn_voltar.setText(QCoreApplication.translate("TelaPaciente", u"Voltar", None))
         self.btn_avancar.setText(QCoreApplication.translate("TelaPaciente", u"Avan\u00e7ar", None))
     # retranslateUi
